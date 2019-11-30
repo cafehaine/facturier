4c97077053055553a70a7e36d5f5754e58161dd3
cafehaine <kilian.guillaume@gmail.com>
2019-11-30 14:43:06 +0100

    Add basic template rendering using weasyprint and jinja

--- facturier/__main__.py
+++ facturier/__main__.py
@@ -1,4 +1,33 @@
+import argparse
+import json
+from datetime import date
+
+from jinja2 import Environment, FileSystemLoader
+from weasyprint import CSS, HTML
+
+
 from facturier import entities
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='commands')
+    reg_parser = subparsers.add_parser('register', aliases=['reg','r'], help="Register a client or a new bill.")
+    list_parser = subparsers.add_parser('list', aliases=['l'], help="List clients or bills.")
+    gen_parser = subparsers.add_parser('generate', aliases=['gen','g'], help="Generate a pdf for one or many bills.")
+    args = parser.parse_args()
+    print(args)
+
+    #TODO Demo code
+    # remove when DB bindings are working, and cli is implemented
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=True
+    )
+
+    template = env.get_template('template.html')
+    with open('config.json') as config:
+        own = json.load(config)
+    rendered_html = template.render(own=own, bill={'date': date.today(),'id':42, 'total':50}, client=own)
+    css = CSS('style.css')
+    HTML(string=rendered_html).write_pdf('/tmp/template.pdf', stylesheets=[css])
+
