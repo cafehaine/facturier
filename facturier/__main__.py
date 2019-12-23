59bc9a2a1f323e65d59909483344ff476dbb9e13
cafehaine <kilian.guillaume@gmail.com>
2019-12-23 14:08:20 +0100

    Start better handling of arguments

--- facturier/__main__.py
+++ facturier/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 from datetime import date
+import sys
 
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import CSS, HTML
@@ -8,41 +9,69 @@ from weasyprint import CSS, HTML
 from facturier import entities
 from facturier import tui
 
+def handle_create(**kwargs):
+    print(kwargs)
+
+def handle_list(**kwargs):
+    print(kwargs)
+
+def handle_generate(**kwargs):
+    print(kwargs)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title='commands')
-    reg_parser = subparsers.add_parser('register',
-                                       aliases=['reg', 'r'],
-                                       help="Register a client or a new bill.")
+
+    # Create new client or bill
+    create_parser = subparsers.add_parser('create',
+                                          aliases=['c'],
+                                          help="Create a client or a new bill.")
+    create_parser.set_defaults(func=handle_create)
+    create_parser.add_argument('type',
+                               nargs=1,
+                               choices=['client', 'c', 'bill', 'b'])
+
+    # List clients or bills
     list_parser = subparsers.add_parser('list',
                                         aliases=['l'],
                                         help="List clients or bills.")
+    list_parser.set_defaults(func=handle_list)
+    list_parser.add_argument('type',
+                             nargs=1,
+                             choices=['client', 'c', 'bill', 'b'])
+
+    # Generate a bill's PDF
     gen_parser = subparsers.add_parser(
         'generate',
         aliases=['gen', 'g'],
         help="Generate a pdf for one or many bills.")
-    args = parser.parse_args()
-    print(args)
+    gen_parser.set_defaults(func=handle_generate)
+
+    namespace = vars(parser.parse_args())
+    if 'func' not in namespace:
+        parser.print_help()
+        sys.exit()
+    namespace['func'](**namespace)
 
     #TODO Demo code
     # remove when DB bindings are working, and cli is implemented
-    entities.DB.bind(provider='sqlite', filename=':memory:')
-    entities.DB.generate_mapping(create_tables=True)
-    entities.generateRandomClients()
-    tui.new_bill()
+    #entities.DB.bind(provider='sqlite', filename=':memory:')
+    #entities.DB.generate_mapping(create_tables=True)
+    #entities.generateRandomClients()
+    #tui.new_bill()
 
-    env = Environment(loader=FileSystemLoader('.'), autoescape=True)
+    #env = Environment(loader=FileSystemLoader('.'), autoescape=True)
 
-    template = env.get_template('template.html')
-    with open('config.json') as config:
-        own = json.load(config)
-    rendered_html = template.render(own=own,
-                                    bill={
-                                        'date': date.today(),
-                                        'id': 42,
-                                        'total': 50
-                                    },
-                                    client=own)
-    css = CSS('style.css')
-    HTML(string=rendered_html).write_pdf('/tmp/template.pdf',
-                                         stylesheets=[css])
+    #template = env.get_template('template.html')
+    #with open('config.json') as config:
+    #    own = json.load(config)
+    #rendered_html = template.render(own=own,
+    #                                bill={
+    #                                    'date': date.today(),
+    #                                    'id': 42,
+    #                                    'total': 50
+    #                                },
+    #                                client=own)
+    #css = CSS('style.css')
+    #HTML(string=rendered_html).write_pdf('/tmp/template.pdf',
+    #                                     stylesheets=[css])
